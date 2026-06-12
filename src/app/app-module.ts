fa126e416a332154daa0ab167e957a34a83fae8c
Victor-Kenji-Hirosue <kenjihvictor@gmail>
2026-06-12 20:34:37 -0300

    Feat: página de Login

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -6,13 +6,15 @@ import { App } from './app';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { TorcedoresComponent } from './torcedores-component/torcedores-component';
 import { FooterComponent } from './footer-component/footer-component';
 import { NavBarComponent } from './nav-bar-component/nav-bar-component';
 import { TimesComponent } from './times-component/times-component';
 import { MatCardModule } from '@angular/material/card';
-import { LoginComponent } from './login-component/login-component';
+import { LoginComponent } from './features/auth/login-component/login-component';
+
 
 @NgModule({
   declarations: [
@@ -30,6 +32,7 @@ import { LoginComponent } from './login-component/login-component';
     MatIconModule,
     MatCardModule,
     NgbModule,
+    ReactiveFormsModule,
   ],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,11 +1,13 @@
-import { LoginComponent } from './login-component/login-component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TorcedoresComponent } from './torcedores-component/torcedores-component';
 import { TimesComponent } from './times-component/times-component';
+import { LoginComponent } from './features/auth/login-component/login-component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent},
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  {path: 'auth/login', component: LoginComponent},
   {path: 'torcedores', component: TorcedoresComponent},
   {path: 'times', component: TimesComponent},
 

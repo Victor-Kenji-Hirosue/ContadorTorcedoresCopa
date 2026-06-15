b34d72ebe66beb7f86ea6ab4a186899cabe08afe
Victor-Kenji-Hirosue <kenjihvictor@gmail>
2026-06-15 01:44:52 -0300

    Inicialização do Spring e Criação da entidade Torcedores

--- src/app/nav-bar-component/nav-bar-component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-nav-bar-component',
-  standalone: false,
-  templateUrl: './nav-bar-component.html',
-  styleUrl: './nav-bar-component.css',
-})
-export class NavBarComponent {}
